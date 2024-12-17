8cb6cc5198cc46ded0786f66cae2143fe00ee216
MH-Lee <lmhoon012@gmail.com>
2024-12-17 10:30:58 +0000

    fixed code

--- src/pretrained_models/transformer.py
+++ src/pretrained_models/transformer.py
@@ -13,7 +13,7 @@ class Embedding(nn.Embedding):
                                         sparse=sparse, _weight=_weight)
 
     def reset_parameters(self):
-        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        nn.init.kaiming_normal_(self.weight)
         if self.padding_idx is not None:
             with torch.no_grad():
                 self.weight[self.padding_idx].fill_(0)
@@ -28,17 +28,18 @@ class BERT(nn.Module):
         self.mlm_loss_type = mlm_loss_type
         if pretrained_emb is None:
             self.code_emb = Embedding(vocab_size, embed_dim, padding_idx=0)
+            self.diag_size = 864
         else:
             self.pretrained_emb = pretrained_emb[:, :embed_dim]
             self.diag_size = self.pretrained_emb.size(0) + 1
             self.code_emb = Embedding(vocab_size, embed_dim, padding_idx=0)
             self.code_emb.weight.data[1:self.diag_size] = self.pretrained_emb
             
-        # self.positional_encoding = PositionalEncoding(embed_dim, dropout_rate, max_len)
-        # self.visit_segment_emb = nn.Embedding(51, embed_dim, padding_idx=50)
-        # nn.init.xavier_normal_(self.visit_segment_emb.weight.data)÷
+        self.positional_encoding = PositionalEncoding(embed_dim, dropout_rate, max_len)
+        self.visit_segment_emb = nn.Embedding(51, embed_dim, padding_idx=50)
+        # nn.init.kaiming_normal_(self.visit_segment_emb.weight.data)        
         self.code_type_emb = nn.Embedding(5, embed_dim, padding_idx=0)
-        nn.init.kaiming_normal_(self.code_type_emb.weight.data)        
+        # nn.init.kaiming_normal_(self.code_type_emb.weight.data)        
         self.time_emb = TimeEncoder(embed_dim, device)
   
         self.act1 = nn.ReLU()
@@ -51,9 +52,8 @@ class BERT(nn.Module):
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
         self.encoder.apply(self.reset_parameters)
         
-        if self.mlm_loss_type == 'ce':
-            self.mlm_layer = nn.Linear(embed_dim, vocab_size)
-        
+        # if self.mlm_loss_type == 'ce':
+        #     self.mlm_layer = nn.Linear(embed_dim, vocab_size)
         self.classify_layer = nn.Linear(embed_dim, num_classes)
         
         self.act2 = nn.GELU()
@@ -84,12 +84,12 @@ class BERT(nn.Module):
     
     def forward(self, batch):
         code_embs = self.code_emb(batch['masked_visit_seq'].to(self.device))
-        # code_embs_pos = self.positional_encoding(code_embs.to(self.device))
-        # visit_seg = self.visit_segment_emb(batch['visit_segments'].to(self.device))
+        code_embs_pos = self.positional_encoding(code_embs.to(self.device))
+        visit_seg = self.visit_segment_emb(batch['visit_segments'].to(self.device))
         code_types = self.code_type_emb(batch['code_types'].to(self.device))
         timedelta_emb = self.time_emb(batch['time_delta'].to(self.device),
                                       batch['seq_mask'].unsqueeze(2).to(self.device))
-        input_embs = code_embs + code_types + timedelta_emb
+        input_embs = code_embs_pos + visit_seg + code_types + timedelta_emb
         
         encoder_output = self.encoder(input_embs, src_key_padding_mask=batch['seq_mask'].to(self.device))
         mlm_pos = batch['mask_pos'][:, :, None].expand(-1, -1, encoder_output.size(-1)).long()
@@ -97,8 +97,7 @@ class BERT(nn.Module):
         h_masked = self.layer_norm(self.act2(self.fc1(h_masked))) # masking position [batch_size, max_pred, d_model]
         
         if self.mlm_loss_type == 'ce':
-            mlm_output = self.mlm_layer(h_masked)
-            # mlm_output = h_masked @ self.code_emb.weight.data.t() # masking position [batch_size, max_pred, vocab_size]
+            mlm_output = h_masked @ self.code_emb.weight.data.t() # masking position [batch_size, max_pred, vocab_size]
         elif self.mlm_loss_type == 'mse':
             mlm_output = h_masked
         else:
